558e0b5a041bc07c9df391741c4a084d4f6ac385
padmashree <padmashree@adobe.com>
2026-02-11 23:29:43 +0530

    [feat] Implement eds header embed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import AEMHeader from "./components/AEMHeader";
+
+function App() {
+  return (
+    <>
+      <AEMHeader />
+      <main>Your React Content</main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/AEMHeader.jsx
@@ -0,0 +1,9 @@
+export default function AEMHeader() {
+    return (
+    <aem-embed
+      class="header"
+      url="https://feature-1670372-header--digital-ecomm-eds--subway-technology.aem.page/nav"
+      type="header"
+    ></aem-embed>
+  );
+}
