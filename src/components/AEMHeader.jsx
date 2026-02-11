bc45452981a1a1afb5bc662ee0049b8d986415d6
padmashree <padmashree@adobe.com>
2026-02-12 00:38:22 +0530

    [deploy] gh pages

--- src/components/AEMHeader.jsx
+++
@@ -1,9 +0,0 @@
-export default function AEMHeader() {
-    return (
-    <aem-embed
-      class="header"
-      url="https://feature-1670372-header--digital-ecomm-eds--subway-technology.aem.page/nav"
-      type="header"
-    ></aem-embed>
-  );
-}
